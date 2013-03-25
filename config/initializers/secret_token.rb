e3b32666a0e0135352a77b7077707123b134d289
Minh D <minhdn@outlook.com>
2013-03-24 22:19:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezblog::Application.config.secret_token = '0b2edd1d1838ff7318db4168c9bd8348d9556e13a243f3d7b91d2ff3b82a30026e297c8f6107873b3b9c2822caa25f213a3530b733ab5249218fa236e8f111b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ezblog::Application.config.session_store :cookie_store, key: '_ezblog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ezblog::Application.config.session_store :active_record_store
